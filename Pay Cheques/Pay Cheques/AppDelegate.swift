2a11eeaa7d4ad8b9c4579ac22ab3984548960867
Albertus Liberius Humaniensis Disciplinus <w890702@gmail.com>
2018-01-15 00:41:09 +0900

    new macosapp - added frameworks

---
+++ Pay Cheques/Pay Cheques/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pay Cheques
+//
+//  Created by WOOHYUN RIM on 2018-01-15.
+//  Copyright © 2018 Protelum Liberium. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
